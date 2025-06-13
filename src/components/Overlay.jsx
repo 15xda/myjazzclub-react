d2484ce16dbc3a8682e5819a9853b25f837c44cc
Aziz Dovudzoda <macintosh@Azizs-MacBook-Pro.local>
2025-06-13 22:14:04 +0500

    Fixed Background

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Overlay = () => {
   return (
     <div className='absolute inset-0 z-100 pointer-events-none'>
-      <img className='h-full w-full object-cover opacity-12' src="public/overlays/noise.gif" alt="" />
-      <img className='h-full w-full object-cover opacity-12' src="public/overlays/dirt.webp" alt="" />
+      <img className='absolute h-full w-full object-cover opacity-12' src="public/overlays/noise.gif" alt="" />
+      <img className='h-full w-full object-cover opacity-10' src="public/overlays/dirt.webp" alt="" />
     </div>
   )
 }
